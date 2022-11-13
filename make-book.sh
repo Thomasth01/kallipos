d05e850f32bd129119eb299f10cb2515348d3e0d
Thomasth01 <p19theo@ionio.gr>
2022-11-13 23:14:29 +0200

    completed script

--- make-book.sh
+++ make-book.sh
@@ -16,5 +16,20 @@ for filename in text/apx*.txt; do
    pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"
 done
 
+pandoc latex/pre.tex latex/intro.tex -o latex/tempintro.tex
 
-sed -i '' 's+Figure+Εικόνα+g' ./latex/ch0*
+pandoc latex/ch0* -o latex/tempchp.tex
+
+pandoc latex/apx* -o latex/tempapx.tex
+
+pandoc latex/epi.tex latex/tempapx.tex -o latex/tempend.tex
+
+pandoc -s latex/tempintro.tex latex/tempchp.tex latex/tempend.tex -o latex/book.tex
+
+rm latex/temp*
+
+sed -i 's/Figure/Εικόνα/g' ./latex/book.tex
+
+sed -i 's/\.\.\/i/i/g' latex/book.tex
+
+pandoc -N --variable mainfont=FreeSerif --variable fontsize=12pt --variable version=1.0 latex/book.tex --pdf-engine=xelatex --toc --toc-depth=2 -o book.pdf
