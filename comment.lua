c6bbcbb4abb8e1ac6b67195092b5d6949ed60977
Thomasth01 <p19theo@ionio.gr>
2022-11-17 22:21:13 +0200

    improved comentation

--- comment.lua
+++ comment.lua
@@ -1,11 +1,11 @@
-function Image(comment)
-      if comment.classes:find('comment',1) then
-        local f = io.open("comments/" .. comment.src, 'r')
-        local doc = pandoc.read(f:read('*a'))
-        f:close()
-        local comment = pandoc.utils.stringify(doc.meta.comments) or "Comment has not been set"
-        local author = pandoc.utils.stringify(doc.meta.author) or "Author has not been set"
-        local commentation = "> " .. comment .. " " .. author
-        return pandoc.RawInline('markdown',commentation)
-      end
+function Para (elem)
+  if #elem.content == 1 and elem.content[1].t == "Image" then
+    local img = elem.content[1]
+    if img.classes[1] == "comment" then
+      local f = io.open("comments/" .. img.src, 'r')
+      local blocks = pandoc.read(f:read('*a')).blocks
+      f:close()
+      return blocks
+    end
+  end
 end
--- make-book.sh
+++ make-book.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #assemble and preprocess all the sources files
 
-pandoc text/pre.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/pre.tex
+pandoc text/pre.txt --lua-filter=epigraph.lua --to markdown | pandoc -s --top-level-division=chapter --to latex > latex/pre.tex
 pandoc text/intro.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/intro.tex
 
 for filename in text/ch*.txt; do
@@ -16,20 +16,4 @@ for filename in text/apx*.txt; do
    pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"
 done
 
-pandoc latex/pre.tex latex/intro.tex -o latex/tempintro.tex
-
-pandoc latex/ch0* -o latex/tempchp.tex
-
-pandoc latex/apx* -o latex/tempapx.tex
-
-pandoc latex/epi.tex latex/tempapx.tex -o latex/tempend.tex
-
-pandoc -s latex/tempintro.tex latex/tempchp.tex latex/tempend.tex -o latex/book.tex
-
-rm latex/temp*
-
-sed -i 's/Figure/Εικόνα/g' ./latex/book.tex
-
-sed -i 's/\.\.\/i/i/g' latex/book.tex
-
-pandoc -N --variable mainfont=FreeSerif --variable fontsize=12pt --variable version=1.0 latex/book.tex --pdf-engine=xelatex --toc --toc-depth=2 -o book.pdf
+sed -i 's/Figure/Εικόνα/g' latex/*.tex
